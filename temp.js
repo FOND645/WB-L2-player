e5e9cc89bb4c7982f0b5a43e535de48bc5c0fcf4
FOND645 <112339957+FOND645@users.noreply.github.com>
2023-12-07 15:35:23 +0300

    Промежуточно

--- temp.js
+++
@@ -1,9 +0,0 @@
-import WaveSurfer from "wavesurfer.js";
-
-const player = WaveSurfer.create({
-    container: this.playerContainerElement,
-    waveColor: 'rgb(200, 0, 200)',
-    progressColor: 'rgb(100, 0, 100)',
-    url: this.getAudioURL(trackName),
-})
-
